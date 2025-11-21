555cc4cbdefe2152fceca9c796dc844db7aec9d0
Riccardo Bergamasco <riccardo_bergamasco@hotmail.it>
2025-11-21 17:33:57 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,13 +1,15 @@
-local script_folder = "/SD/SCRIPTS/TELEMETRY/MYPRACTICE"  
+local script_folder = "/SCRIPTS/TELEMETRY/MYPRACTICE"  
 local NumberOfOptions= 4
 local currentOption = 1
 local SelectionIsDone = false
+local myscripts={}
+myscripts[2]="field.lua"
 
 
 local function drawMenuScreen()
     lcd.clear()
     lcd.drawText(1,1,"1: pippo",(currentOption == 1 and INVERS or 0))
-    lcd.drawText(1,12,"2: pluto",(currentOption == 2 and INVERS or 0))
+    lcd.drawText(1,12,"2: field",(currentOption == 2 and INVERS or 0))
     lcd.drawText(1,24,"3: topolino",(currentOption == 3 and INVERS or 0))
     lcd.drawText(1,36,"4: paperino",(currentOption == 4 and INVERS or 0))
 end
@@ -28,7 +30,7 @@ local function run(event)
             SelectionIsDone = true
         end
     elseif SelectionIsDone then
-        lcd.drawText(10,10,"hai scelto"..currentOption)        
+        loadScript(script_folder.."/"..myscripts[currentOption])()        
     end
 end
 
