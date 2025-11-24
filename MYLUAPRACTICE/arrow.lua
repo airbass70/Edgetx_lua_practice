db200ffbd2a0cbd4a353873893de5972303d6dfd
Riccardo Bergamasco <riccardo_bergamasco@hotmail.it>
2025-11-24 17:43:36 +0100

    Add telemetry script for graphical arrow indicator
    Initial commit of a telemetry script for drawing a graphical arrow indicator using rotation matrix. The script is designed for use with Radiomaster Boxer and takes input from the rudder stick to determine rotation angle.

---
+++ MYLUAPRACTICE/arrow.lua
@@ -0,0 +1,35 @@
+--[[
+================
+==NEEDLE      ==
+================
+rev 1.1 27 Feb 2025
+	Riccardo Airbass
+Telemetry script esercitazione sul comando draw line e la matrice di rotazione. Disegna l'indicatore grafico a freccia da usare come indicatore di rilevamento polare (per es. home). Provato su Radiomaster Boxer. Input angolo rotazione con stick rudder
+--]]
+
+
+if firstRun then
+  RudId = getFieldInfo("rud").id
+  --local arrowPoints={0,1,0.7,-0.7,0,-0.3,-0.7,-0.7,0,1}  --  CURSORE coordinate vertici  (x1,y1,...xn,yn)
+  arrowPoints={0,1,1,0,0.4,0,0.4,-1,-0.4,-1,-0.4,0,-1,0,0,1}  -- FRECCIA coordinate vertici  (x1,y1,...xn,yn)
+  arrow={}
+  arrowPos={82,8}
+  arrowScale=6
+  arrowResized={}
+  local arrowRotaded={}
+  firstRun = false
+end
+local bearing = getValue(RudId)/1024*180
+local rndBearing=math.floor(bearing+180+0.5)   --arrotonda all'intero più vicino. Solo per numeri positivi.
+local theta=math.rad(rndBearing)
+for i=1,#arrowResized-1,2 do
+  arrowRotaded[i]=arrowResized[i]*math.cos(theta)-arrowResized[i+1]*math.sin(theta)
+  arrowRotaded[i+1]=arrowResized[i]*math.sin(theta)+arrowResized[i+1]*math.cos(theta)
+end
+for i=1,#arrowRotaded-1,2 do
+  arrow[i]=arrowRotaded[i]+arrowPos[1]
+  arrow[i+1]=arrowRotaded[i+1]+arrowPos[2]
+end
+for i=1,#arrow-3,2 do
+  lcd.drawLine(arrow[i],arrow[i+1],arrow[i+2],arrow[i+3],SOLID,FORCE)
+end
