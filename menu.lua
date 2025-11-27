84586b9ef50f1aa5df6d62474050cf292dec3a4a
Riccardo Bergamasco <riccardo_bergamasco@hotmail.it>
2025-11-27 12:47:27 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,29 +1,31 @@
 local script_folder = "/SCRIPTS/TELEMETRY/MYPRACTICE"  
-local numberOfOptions= 4
 local currentOption = 1
 local selectionIsDone = false
-local myscripts={}
-myscripts[2]="field.lua"
+local myscripts={"field.lua"}
+local executeScript={}
+local numberOfOptions=#myscripts
+
 
 
 local function drawMenuScreen()
     lcd.clear()
-    lcd.drawText(1,1,"1: pippo",(currentOption == 1 and INVERS or 0))
-    lcd.drawText(1,12,"2: field",(currentOption == 2 and INVERS or 0))
-    lcd.drawText(1,24,"3: topolino",(currentOption == 3 and INVERS or 0))
-    lcd.drawText(1,36,"4: paperino",(currentOption == 4 and INVERS or 0))
+    for i=1,numberOfOptions do
+        lcd.drawText(1,1+12*(i-1),"1: "..myscripts[i],(currentOption == i and INVERS or 0))
+    end
 end
 
-local executeScript=loadScript(script_folder.."/"..myscripts[currentOption])()
+for i=1,numberOfOptions do
+    executeScript[i]=loadScript(script_folder.."/"..myscripts[i])()
+end
 
 local function run(event)
     if not selectionIsDone then
         if event == EVT_ROT_RIGHT then
             currentOption=currentOption + 1
-            if currentOption>NumberOfOptions then currentOption=1 end
+            if currentOption>numberOfOptions then currentOption=1 end
         elseif event == EVT_ROT_LEFT then
             currentOption=currentOption - 1
-            if currentOption<1 then currentOption=NumberOfOptions end
+            if currentOption<1 then currentOption=numberOfOptions end
         end
         drawMenuScreen()
         if event == EVT_ROT_BREAK then
@@ -32,7 +34,7 @@ local function run(event)
             selectionIsDone = true
         end
     elseif selectionIsDone then
-        executeScript()
+        executeScript[currentOption]()
         if event == EVT_EXIT_BREAK then SelectionIsDone = false end
     end
 end
